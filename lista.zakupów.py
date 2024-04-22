83be092f022ba526c28ab22bc55adb7aabf694b9
Antek Pomocka <antonipomocka@mailplus.pl>
2024-04-22 11:22:48 +0200

    new1

--- lista.zakupów.py
+++ lista.zakupów.py
@@ -6,3 +6,5 @@ print ("lista zakupów:")
 print(f"Idę do Warzywniak, kupuję tu następujące rzeczy: {', '.join([item.capitalize() for item in dict['warzywniak']])}.")
 print(f"Idę do Piekarnia, kupuję tu następujące rzeczy: {', '.join([item.capitalize() for item in dict['piekarnia']])}.")
 print (f"W sumie kupuję {len(dict['warzywniak']) + len(dict['piekarnia'])} produktów")
+
+print ("dowolna zmiana1")
